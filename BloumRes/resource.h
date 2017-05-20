48dae89d18e0607be6262077f4ae1ae42719f112
gbrfix <gabriel.ruffix@gmail.com>
2017-05-20 15:24:12 +0200

    ajout projet

---
+++ BloumRes/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by BloumRes.rc
+//
+#define IDB_BACKGROUND              101
+#define IDB_CURSOR                  102
+#define IDB_MAP                     103
+#define IDB_SQUARE                  104
+#define IDB_TRIANGLE_LOOP           105
+#define IDB_TRIANGLE_STOP           106
+#define IDB_WALL                    107
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_HEADER
+
+#define MAIN_HEADER 1
+#define NB_WALL 9
+
+enum ANIMATION
+{
+	STOP,
+	LOOP
+};
+
+#endif
